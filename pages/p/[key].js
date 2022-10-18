6ee1066f75f8bc5de866378d9024e921136faac1
Duy Quang <dquang1305@gmai.com>
2022-10-18 12:07:06 -0700

    add product to footer url p

--- pages/p/[key].js
+++ pages/p/[key].js
@@ -1,35 +1,40 @@
-import React, { Component } from 'react';
-import Navbar from '../../components/Layouts/Navbar';
-import PageBanner from '../../components/Common/PageBanner';
-import Footer from '../../components/Layouts/Footer';
-import { getPageContents, getHeader } from '../api';
-import { createMarkupNormal } from '../../utils/auth.helper';
-import Head from 'next/head';
+import React, { Component } from "react";
+import Navbar from "../../components/Layouts/Navbar";
+import PageBanner from "../../components/Common/PageBanner";
+import Footer from "../../components/Layouts/Footer";
+import { getPageContents, getHeader, getAllNodeProducts } from "../api";
+import { createMarkupNormal } from "../../utils/auth.helper";
+import Head from "next/head";
 
 export async function getStaticProps({ params }) {
-  const [res, headers] = await Promise.all([getPageContents(params.key), getHeader()])
+  const [res, headers, allNodeProducts] = await Promise.all([
+    getPageContents(params.key),
+    getHeader(),
+    getAllNodeProducts(),
+  ]);
   return {
     props: {
       data: res?.data?.data,
-      headers: headers?.data?.data
-    }
-  }
+      headers: headers?.data?.data,
+      allNodeProducts: allNodeProducts?.data?.data,
+    },
+  };
 }
 
 export async function getStaticPaths() {
   return {
     paths: [
-      { params: { key: 'dieu-khoan' } },
-      { params: { key: 'chinh-sach-bao-mat' } },
-      { params: { key: 'chinh-sach-thanh-toan' } },
-      { params: { key: 'chinh-sach-giao-hang' } },
-      { params: { key: 'chinh-sach-doi-tra' } },
+      { params: { key: "dieu-khoan" } },
+      { params: { key: "chinh-sach-bao-mat" } },
+      { params: { key: "chinh-sach-thanh-toan" } },
+      { params: { key: "chinh-sach-giao-hang" } },
+      { params: { key: "chinh-sach-doi-tra" } },
     ],
-    fallback: false
+    fallback: false,
   };
 }
 
-const TermsConditions = ({ data, headers }) => {
+const TermsConditions = ({ data, headers, allNodeProducts }) => {
   return (
     <>
       <Head>
@@ -39,30 +44,34 @@ const TermsConditions = ({ data, headers }) => {
 
       <div className="text-container mt-5 ptb-100">
         <div className="container">
-          {data
-            ? (
-              <>
-                {/* <img src="/images/comming.jpg" alt="comming-soon" /> */}
-                < h2 className="text-left uppercase font-weight-bold text-header" style={{ color: "#167ffc" }}>
-                  {data.name}
-                </h2>
-                <div className="pt-5">
-                  <div
-                    dangerouslySetInnerHTML={createMarkupNormal(data.description)}
-                    className="text-gray"
-                  />
-                </div>
-              </>
-            )
-            : (<div>Xin lỗi chúng tôi không tìm thấy nội dung này. Quay về <Link href="/">Trang chủ</Link></div>)
-          }
+          {data ? (
+            <>
+              {/* <img src="/images/comming.jpg" alt="comming-soon" /> */}
+              <h2
+                className="text-left uppercase font-weight-bold text-header"
+                style={{ color: "#167ffc" }}
+              >
+                {data.name}
+              </h2>
+              <div className="pt-5">
+                <div
+                  dangerouslySetInnerHTML={createMarkupNormal(data.description)}
+                  className="text-gray"
+                />
+              </div>
+            </>
+          ) : (
+            <div>
+              Xin lỗi chúng tôi không tìm thấy nội dung này. Quay về{" "}
+              <Link href="/">Trang chủ</Link>
+            </div>
+          )}
         </div>
       </div>
 
-      <Footer />
+      <Footer product={allNodeProducts} />
     </>
   );
-
-}
+};
 
 export default TermsConditions;
