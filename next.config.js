0f67f733996df77ab43a85bcab2dd4fb018e9c25
thanleanhtuan <thanleanhtuan4@gmail.com>
2022-10-21 08:07:17 +0700

    updated

--- next.config.js
+++ next.config.js
@@ -20,58 +20,66 @@
 
 // the following 2 lines are specific to this repo. in your project use next-with-less directly
 const withLess = require("next-with-less");
-const path = require('path')
-const fs = require('fs');
-const lessToJS = require('less-vars-to-js');
+const path = require("path");
+const fs = require("fs");
+const lessToJS = require("less-vars-to-js");
 
 // const withLess = require("../../src");
 withLess.patchNext(require("next/dist/build/webpack/config/blocks/css"));
 const themeVariables = lessToJS(
-  fs.readFileSync(path.resolve(__dirname, './styles/antd-custom.less'), 'utf8'),
+  fs.readFileSync(path.resolve(__dirname, "./styles/antd-custom.less"), "utf8")
 );
 // const withBundleAnalyzer = require("@next/bundle-analyzer")({
 //   enabled: process.env.ANALYZE === "true",
 // })
-module.exports = 
-// withBundleAnalyzer(
+module.exports =
+  // withBundleAnalyzer(
   withLess({
-  reactStrictMode: true,
-  lessLoaderOptions: {
-    lessOptions: {
-      // it's possible to use additionalData or modifyVars for antd theming
-      // read more @ https://ant.design/docs/react/customize-theme
-      javascriptEnabled: true,
-      modifyVars: themeVariables, // make your antd custom effective
-    }
-  },
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-  images: {
-    domains: ['docs-dev.ezin.vn', 'localhost', 'images.dmca.com', 'api.ezin.vn', 'ss-images.saostar.vn', 'sandbox.vnpayment.vn', 'sandbox.ezin.vn'],
-  },
-  i18n: {
-    locales: ['vi-VN'],
-    defaultLocale: 'vi-VN'
-  },
-  async redirects() {
-    return [
-      {
-        source: '/a/:id',
-        destination: '/kich-hoat-the/:id',
-        permanent: true,
+    reactStrictMode: true,
+    lessLoaderOptions: {
+      lessOptions: {
+        // it's possible to use additionalData or modifyVars for antd theming
+        // read more @ https://ant.design/docs/react/customize-theme
+        javascriptEnabled: true,
+        modifyVars: themeVariables, // make your antd custom effective
       },
-      {
-        source: '/san-pham/tncn',
-        destination: '/san-pham/binh-an',
-        permanent: true,
-      },
-      {
-        source: '/0988136833',
-        destination:'/profile/tai-tuc',
-        permanent: true,
-      }
-    ]
-  },
-})
+    },
+    sassOptions: {
+      includePaths: [path.join(__dirname, "styles")],
+    },
+    images: {
+      domains: [
+        "docs-dev.ezin.vn",
+        "localhost",
+        "images.dmca.com",
+        "api.ezin.vn",
+        "ss-images.saostar.vn",
+        "sandbox.vnpayment.vn",
+        "sandbox.ezin.vn",
+      ],
+    },
+    i18n: {
+      locales: ["vi-VN"],
+      defaultLocale: "vi-VN",
+    },
+    async redirects() {
+      return [
+        {
+          source: "/a/:id",
+          destination: "/kich-hoat-the/:id",
+          permanent: true,
+        },
+        {
+          source: "/san-pham/tncn",
+          destination: "/san-pham/binh-an",
+          permanent: true,
+        },
+        {
+          source: "/0988136833",
+          destination: "/profile/tai-tuc",
+          permanent: true,
+        },
+      ];
+    },
+  });
 // );
